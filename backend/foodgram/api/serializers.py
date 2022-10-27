c79c7fbade4a33b0184f2d320de84a273f6b7521
expertnsk <expertnsk89@gmail.com>
2022-10-27 14:12:00 +0700

    Changes for 1st review

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -192,7 +192,7 @@ class CreateRecipeSerializer(serializers.ModelSerializer):
             RecipeIngredient(
                 recipe=recipe,
                 amount=ingredient['amount'],
-                ingredient=ingredient['ingredient'],
+                ingredient=Ingredient.objects.get(id=ingredients['id']),
             ) for ingredient in ingredients
         ])
 
